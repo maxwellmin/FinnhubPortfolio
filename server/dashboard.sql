07c79f4c5a8ea84bdd8ecfc8f0100bda8f4e3b98
Cary Huang <huangjn.cary8888@gmail.com>
2024-08-28 22:10:22 +0800

    updated sql database setup

--- server/dashboard.sql
+++ server/dashboard.sql
@@ -16,17 +16,30 @@ CREATE TABLE portfolio (
 );
 
 -- Create the 'transaction' table
-  CREATE TABLE transaction (
+CREATE TABLE transaction (
   transaction_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  asset_id INT NOT NULL,
+  ticker VARCHAR(45) NOT NULL,
   transaction_type VARCHAR(45) NOT NULL,
   transaction_quantity DECIMAL(10,2) NOT NULL,
   transaction_price DECIMAL(10,2) NOT NULL,
   transaction_value DECIMAL(10,2) AS (transaction_quantity * transaction_price) STORED,
-  transaction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (asset_id) REFERENCES portfolio(asset_id)
+  transaction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- Insert sample data into the 'portfolio' table
 INSERT INTO portfolio (asset_type, ticker, quantity, purchase_price)
-VALUES ('Stock', 'APPL', '3', '150.00');
+VALUES ('Stock', 'AAPL', 3, 150.00);
+
+-- Create a trigger to delete rows from 'portfolio' table when quantity becomes 0
+DELIMITER //
+
+CREATE TRIGGER delete_portfolio_on_zero_quantity
+BEFORE UPDATE ON portfolio
+FOR EACH ROW
+BEGIN
+  IF NEW.quantity = 0 THEN
+    DELETE FROM portfolio WHERE asset_id = OLD.asset_id;
+  END IF;
+END //
+
+DELIMITER ;
